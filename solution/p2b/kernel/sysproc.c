d11dd5327185520fbe87b05eb0ff0591eb96d8bf
Ryan Ruenroeng <ruenroeng@royal-23.cs.wisc.edu>
2020-11-19 00:02:41 -0600

    Add solutions.

---
+++ solution/p2b/kernel/sysproc.c
@@ -0,0 +1,122 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "param.h"
+#include "mmu.h"
+#include "proc.h"
+#include "sysfunc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return proc->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = proc->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+  
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(proc->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since boot.
+int
+sys_uptime(void)
+{
+  uint xticks;
+  
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+int sys_getpinfo(void)
+{
+  struct pstat* status;
+  if (argptr(0, (char**)&status, sizeof(struct pstat)) < 0)
+    return -1;
+  if (!status)
+    return -1;
+
+  memset(status, 0, sizeof(struct pstat));
+  return getpinfo(status);
+}
+
+int sys_setpri(void)
+{
+  int pid, pri;
+  if (argint(0, &pid) < 0 || argint(1, &pri) < 0)
+    return -1;
+  if (pri < 0 || pri > 3)
+    return -1;
+  
+  return setpri(pid, pri);
+}
+
+int sys_getpri(void)
+{
+  int pid;
+  if (argint(0, &pid) < 0)
+    return -1;
+
+  return getpri(pid);
+}
